39a67cbc0d862293defef4d6fe81f4b1322d754e
29942016 <oliver.buckler@gmail.com>
2022-05-08 06:42:42 +0800

    Update nuke.lua

--- nuke.lua
+++ nuke.lua
@@ -1,8 +1,7 @@
 iterate = function(folder)
 	bins = folder.get_files
 	for bin in bins
-		if bin.name == "p3n71n3l" then
-			print("=>" + bin.name)
+		if bin.name == "testxyz" then
 			n = bin.name
 			r = bin.delete
 			if r.len > 0 then
@@ -10,15 +9,13 @@ iterate = function(folder)
 			else
 				print("File deleted OK " + n)
 			end if
-			print("==")
 		end if
 	end for
 	folders = folder.get_folders
 	for f in folders
-		printFiles(f)
+		iterate(f)
 	end for
 end function
   
-  fRoot = result.File("/")
-	iterate(fRoot)
-  
+fRoot = get_shell.host_computer.File("/")
+iterate(fRoot)
